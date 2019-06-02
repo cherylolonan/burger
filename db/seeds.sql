d79145b7ea6461789ca0e16ad902bfb8ca913ce8
shrill <cheryl.olonan@gmail.com>
2019-06-01 17:27:24 -0700

    Worked on App, DB, Config set up

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Bacon Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Guacamole Bacon Burger", false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Plain Cheeseburger", false);
