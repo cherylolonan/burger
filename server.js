0c73d1cb6e416871432dc2602a1b9dad5e7f5bad
shrill <cheryl.olonan@gmail.com>
2019-06-01 21:21:36 -0700

    Fixed MIME-type error with css

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require("express");
+var path = require('path')
 var PORT = process.env.PORT || 8080;
 var app = express();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -11,7 +12,7 @@ var expressHandlebars = require("express-handlebars");
 app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burger_controllers.js");
+var routes = require("./controllers/burgers_controller");
 
 app.use(routes);
 app.listen(PORT, function() {
